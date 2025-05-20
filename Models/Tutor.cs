0f8b2cc893cad8ad70ebaae4d1b00d8dc1320530
1Patricio <andersongpatricio@hotmail.com>
2025-05-20 19:44:00 -0300

    Models

---
+++ Models/Tutor.cs
@@ -0,0 +1,8 @@
+public class Tutor
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = "";
+    public string Telefone { get; set; } = "";
+    public string Email { get; set; } = "";
+
+}
