9718a64bcc45dcdf398a504ebd234c86100377be
1Patricio <andersongpatricio@hotmail.com>
2025-05-27 21:23:27 -0300

    Requireds em atributos obrigatórios dentro da Model

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Pet
 {
     public int Id { get; set; }
     public string Nome { get; set; } = "";
+    [Required]
     public string Especie { get; set; } = "";
+    [Required]
     public string Raca { get; set; } = "";
     public int TutorId { get; set; }
 }
